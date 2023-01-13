dfada9660753eb9c8ccc235635d7c9e933f37fec
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-01-13 16:30:48 +0530

    COmmit

---
+++ src/main/java/com/lti/springboot/demo/security/keycloak/KeycloakSecurityConfig.java
@@ -0,0 +1,46 @@
+package com.lti.springboot.demo.security.keycloak;
+
+import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(jsr250Enabled = true)
+public class KeycloakSecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		super.configure(http);
+		http.requestMatchers()
+		.antMatchers("/emp/**")
+		.and().authorizeRequests()
+				.antMatchers("/emp/**").authenticated();
+		http.csrf().disable();
+	}
+
+	@Override
+	@Bean
+	protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+		return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+
+	}
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) {
+		KeycloakAuthenticationProvider keycloakAuthenticationProvider = keycloakAuthenticationProvider();
+		keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(new SimpleAuthorityMapper());
+		auth.authenticationProvider(keycloakAuthenticationProvider);
+	}
+
+}
--- src/main/java/com/lti/springboot/demo/security/model/ERole.java
+++
@@ -1,7 +0,0 @@
-package com.lti.springboot.demo.security.model;
-
-public enum ERole {
-	  ROLE_USER,
-	  ROLE_MODERATOR,
-	  ROLE_ADMIN
-	}
